c0789176ae7697076ff6593fd3f8a016347cec5d
unknown <opokudavid446@gmail.com>
2020-08-18 15:59:18 +0100

    :wrench:

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -16,6 +16,8 @@ export default function useCachedResources() {
           ...MaterialCommunityIcons.font,
           "SFProDisplay-Bold": require("../assets/fonts/SFProDisplay-Bold.ttf"),
           "SFProDisplay-Regular": require("../assets/fonts/SFProDisplay-Regular.ttf"),
+          "SFProDisplay-SemiBold": require("../assets/fonts/SFProDisplay-SemiBold.ttf"),
+          "SFProDisplay-Medium": require("../assets/fonts/SFProDisplay-Medium.ttf"),
         });
       } catch (e) {
         console.warn(e);
